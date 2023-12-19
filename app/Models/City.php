31b01c43dbe5d502a0859959edd8faa998f2d2fe
tamaaja <rizkyputra1206@gmail.com>
2023-12-19 13:15:30 +0800

    hai

---
+++ app/Models/City.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class City extends Model
+{
+    use HasFactory;
+    protected $guarded = [];
+    public function cities(): BelongsTo
+    {
+        return $this->belongsTo(Province::class);
+    }
+}
